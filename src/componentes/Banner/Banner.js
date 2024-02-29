3b70cc90f6d82b0d48a74d75bf98f261a3b5f94d
Camila Sayuri <tokubo.camila@gmail.com>
2024-02-29 11:15:06 -0300

    remoção do teste

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,16 +1,12 @@
-import { Fragment } from 'react'
 import './Banner.css'
 
 function Banner() {
     // JSX: é como o React lê o return e transforma em elementos do DOM
     return (
         //O que o React faz por debaixo dos panos é entender isso aqui e fazer o append no DOM
-        <> 
-            <header className="banner"> 
-                <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
-            </header>
-            <h1>Teste</h1>
-        </>
+        <header className="banner"> 
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
     )
 }
 
